7529a5221493ed466d88e680c5eb53594fc36992
Ewelina Mężyk <ewelinamezykmail@gmail.com>
2022-01-18 11:25:39 +0000

    Login page design

--- src/components/AdminPage/AdminPanel/AdminPanel.js
+++ src/components/AdminPage/AdminPanel/AdminPanel.js
@@ -1,5 +1,28 @@
+import styles from "./AdminPanel.module.css";
+import { Icon, InlineIcon } from "@iconify/react";
+
 const AdminPanel = () => {
-  return <div>AdminPanel</div>;
+  return (
+    <div className={styles.container}>
+      <Icon icon="simple-icons:codechef" className={styles.logo} />
+      <h1 className={styles.title}> Paradiso Italiano</h1>
+      <div className={styles.inputContainer}>
+        <input
+          className={`${styles.input} ${styles.email}`}
+          type="email"
+          placeholder="e-mail"
+        />
+        <input
+          className={`${styles.input} ${styles.password}`}
+          type="password"
+          placeholder="password"
+        />
+        <button className={styles.button} type="submit">
+          Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AdminPanel;
