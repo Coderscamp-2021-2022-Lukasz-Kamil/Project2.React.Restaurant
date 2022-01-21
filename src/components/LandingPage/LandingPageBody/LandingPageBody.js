11581b3168d12311896116d217ba0475837f2962
Your Name <donata1989@gmail.com>
2022-01-21 20:42:20 +0100

    Fix the section about in a mobile version

--- src/components/LandingPage/LandingPageBody/LandingPageBody.js
+++ src/components/LandingPage/LandingPageBody/LandingPageBody.js
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap";
+import styles from  "./LandingPageBody.module.css"
 
 const LandingPageBody = () => {
 	return (
@@ -7,17 +8,15 @@ const LandingPageBody = () => {
 				className='bg-transparent text-white text-center align-self-center'
 				style={{ position: "absolute" }}>
 				<Card.Body>
-					<Card.Title
+					<Card.Title className={styles["title"]}
 						style={{
-							fontFamily: "Italianno",
 							lineHeight: "0.9",
 							fontSize: "clamp(6rem, 16vw, 9rem)",
 						}}>
 						Paradiso Italiano
 					</Card.Title>
-					<Card.Subtitle
+					<Card.Subtitle className={styles["subtitle"]}
 						style={{
-							fontFamily: "Rufina",
 							fontSize: "clamp(1rem, 5vw, 3rem)",
 							marginBottom: "2rem",
 						}}>
@@ -25,18 +24,11 @@ const LandingPageBody = () => {
 					</Card.Subtitle>
 				</Card.Body>
 
-				<Card.Body
-					className='w-50 align-self-center rounded'
-					style={{
-						maxWidth: "60rem",
-						background: "rgba(0, 0, 0, 0.4)",
-						textAlign: "justify",
-					}}>
-					<Card.Title
-						style={{ fontFamily: "Montserrat", textTransform: "uppercase" }}>
+				<Card.Body  className={styles["about"]}>
+					<Card.Title className={styles["aboutTitle"]}>
 						About
 					</Card.Title>
-					<Card.Text style={{ fontFamily: "Montserrat" }}>
+					<Card.Text>
 						With this desire was born the idea of ​​creating Paradiso Italiano,
 						where you can find real Italian flavors: pizza on a delicate, thin
 						crust wood-fired oven, pasta according to Italian recipes and many
