c18c39ca251d83efe8b553e2643e122bc4f7c6aa
Asia Czyżewska <jczyzewska96@gmail.com>
2022-01-25 23:54:52 +0100

    RES-017-backend-frontend-config

--- src/components/MenuPage/MenuPage.js
+++ src/components/MenuPage/MenuPage.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import DishesData from '../DishesData/DishesData'
+
 const MenuPage = () => {
   return (
     <div>
       <div>MenuPage</div>
+      <DishesData/>
     </div>
   );
 };
